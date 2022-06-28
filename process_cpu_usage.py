c607414d769bc67ec91de874cfc6c029d74f3586
Barry Xu <barry.xu@sony.com>
2022-06-28 17:57:13 +0800

    Add usage and save file feature
    Signed-off-by: Barry Xu <barry.xu@sony.com>

--- process_cpu_usage.py
+++ process_cpu_usage.py
@@ -1,16 +1,29 @@
+#!/usr/bin/python3
+
 import psutil
 import sys
+import os
+
+if len(sys.argv) != 2 and len(sys.argv) !=3 :
+  print("Usage: {} PID SAMPLING_TIME [SAVE_FILENAME]".format(sys.argv[0]))
+  print("       PID: Process ID to be checked")
+  print("       SAMPLING_TIME: sampling time (minute)")
+  print("       SAVED_FILENAME: recorded data filename")
+  sys.exit(1)
 
 pid = sys.argv[1]
-check_time = sys.argv[2]
+sampling_time = sys.argv[2]
+save_filename = 'recorded_data.csv'
+if sys.argv[3] :
+  save_filename = sys.argv[3]
 
-print("Pid: {}, Time: {}m".format(pid, check_time))
+print("Pid: {}, Time: {}m".format(pid, sampling_time))
 
 process = psutil.Process(int(pid))
 
 test_result = []
 
-check_number = int(check_time) * 60 # interval is 1s
+check_number = int(sampling_time) * 60 # interval is 1s
 
 for _ in range(0, check_number, 1):
   test_result.append(process.cpu_percent(1))
@@ -20,10 +33,18 @@ for item in test_result:
   summary += item
 
 average_cpu_usage = summary / len(test_result)
+print("CPU usage: {:.0%} in {} minutes".format(average_cpu_usage, sampling_time))
 
 print("Total recorded data: {}".format(len(test_result)))
-for cpu_usage in test_result:
-  print("{:.0%}".format(cpu_usage))
+with open(save_filename, 'w') as f:
+  for cpu_usage in test_result:
+    f.write('{},'.format(cpu_usage))
 
-print("CPU usage: {:.0%} in {} minutes".format(average_cpu_usage, check_time))
+# Remove last ','
+with open(save_filename, 'rb+') as f:
+  f.seek(-1, os.SEEK_END)
+  f.truncate()
+
+print("Save recorded data into {}.".format(save_filename));
+
 
